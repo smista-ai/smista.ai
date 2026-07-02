bf136121659dc5e5bf3995a73cb45a0448862ec7
veeso <christian.visintin@veeso.dev>
2026-07-02 17:40:47 +0200

    feat(cli): add router status command
    Closes: #256

---
+++ crates/smista-cli/src/command/status.rs
@@ -0,0 +1,44 @@
+//! `status` command implementation.
+//!
+//! Gets the status of the smista.ai router and print status.
+
+use anyhow::Context as _;
+use smista_sdk::client::{Client as _, ROUTER_DEFAULT_URL, ReqwestClient, RouterClientConfig};
+use url::Url;
+
+use crate::args::StatusArgs;
+
+pub async fn run(StatusArgs { url }: StatusArgs) -> anyhow::Result<()> {
+    // resolve url; if specified, use it; otherwise, load the CLI config
+    let url = match url {
+        Some(url) => url,
+        None => {
+            let config = crate::config::load_and_validate(&std::env::current_dir()?)
+                .context("Failed to load CLI configuration")?;
+
+            let url = config
+                .router
+                .url
+                .unwrap_or_else(|| ROUTER_DEFAULT_URL.to_string());
+
+            Url::parse(&url).context("Failed to parse router URL")?
+        }
+    };
+
+    let config = RouterClientConfig::new(url.clone());
+    let reqwest_client = ReqwestClient::new(config)?;
+
+    let status = reqwest_client
+        .status()
+        .await
+        .context("Failed to get router status")?;
+
+    println!(
+        r#"smista.ai router at "{url}" status: {status}; version: {version}"#,
+        url = url,
+        status = status.status,
+        version = status.version
+    );
+
+    Ok(())
+}
